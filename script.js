11aafc4b32d5ded2e6c38715df5b635f44915b4d
gokul <gokulpandiyan6@gmail.com>
2024-09-21 20:22:49 +0530

    new changes

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function toggleDropdown() {
     var dropdown = document.getElementsByClassName("dropdown-content")[0];
+    var ex4 = document.getElementById("ex4");
     if (dropdown.style.display === "none" || dropdown.style.display === "") {
         dropdown.style.display = "flex";
         dropdown.style.flexDirection = "column";
-        dropdown.style.gap = "5px";
+        dropdown.style.gap = "6px";
+
+        ex4.style.borderBottom = "solid";
+        ex4.style.borderWidth = "5px";
+        ex4.style.borderBottomColor = "black";
+
     } else {
         dropdown.style.display = "none";
+        ex4.style.borderBottom = "None";
     }
 }
 
